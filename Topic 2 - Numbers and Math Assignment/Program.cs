a49df043d424e9317386c8feb1efb6232be71c2f
Raine DeVries <362571390@GoTVDSB.ca>
2024-09-06 09:10:44 -0400

    Add project files.

---
+++ Topic 2 - Numbers and Math Assignment/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("I will now count my chickens:");
+Console.WriteLine("Hens" + (10 + 5 * 2));
+Console.WriteLine("Roosters" + (25 + 30 / 6));
