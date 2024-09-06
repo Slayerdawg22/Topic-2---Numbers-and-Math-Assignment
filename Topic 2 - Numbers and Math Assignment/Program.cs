ad5702a90b55ce7a38a91bbba5f52c6cb6713985
Raine DeVries <362571390@GoTVDSB.ca>
2024-09-06 09:41:40 -0400

    almost

--- Topic 2 - Numbers and Math Assignment/Program.cs
+++ Topic 2 - Numbers and Math Assignment/Program.cs
@@ -2,3 +2,12 @@
 Console.WriteLine("I will now count my chickens:");
 Console.WriteLine("Hens" + (10 + 5 * 2));
 Console.WriteLine("Roosters" + (25 + 30 / 6));
+Console.WriteLine("six" + 6);
+
+Console.WriteLine("Now I will count the eggs");
+Console.WriteLine(11.0 / 2);
+
+Console.WriteLine("Is it true that 3+2<5-7?");
+
+Console.WriteLine(3 + 2 < 5 - 7);
+
