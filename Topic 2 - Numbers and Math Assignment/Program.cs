62e7024f01915742f09115910832e71d67f82e18
Raine DeVries <362571390@GoTVDSB.ca>
2024-09-16 08:21:39 -0400

    finished

--- Topic 2 - Numbers and Math Assignment/Program.cs
+++ Topic 2 - Numbers and Math Assignment/Program.cs
@@ -11,3 +11,11 @@ Console.WriteLine("Is it true that 3+2<5-7?");
 
 Console.WriteLine(3 + 2 < 5 - 7);
 
+Console.WriteLine("What is 3+2" + (3 + 2));
+Console.WriteLine("What is 5-7" + (5 - 7));
+Console.WriteLine("Oh that's why it's" + (3 + 2 < 5 - 7));
+
+Console.WriteLine(8 * 21);
+Console.WriteLine("Eggs cost 6$ for 12 eggs " + (6.0 / 12).ToString("C"));
+Console.WriteLine("Tax on 79.99 would be " + (79.99 * 0.13).ToString("C"));
+Console.WriteLine(5.6 * 2.54);
